ae3716177be7db42fffb1fb92c2f29f401e648ad
KHMaechel <kimmaechel@gmail.com>
2023-11-06 14:25:01 +0100

    Machine control methods. You can reset, start, stop, abort and clear the machine

--- semesterproject3/semest3/src/main/java/org/example/Main.java
+++ semesterproject3/semest3/src/main/java/org/example/Main.java
@@ -10,19 +10,27 @@ public class Main {
         machineController.readStateCurrent();
 
         /*
+        // read values
         machineController.readMachineSpeedCurrent();
         machineController.readBatchIdCurrent();
         machineController.readQuantityCurrent();
         machineController.readRecipeCurrent();
         */
 
+        // Config batch
+        //*
         machineController.writeQuantityValue(100);
         machineController.writeMachineSpeedValue(20);
-        machineController.writeBatchIdValue(2);
         machineController.writeRecipeValue(1);
+        //*/
 
 
+        // control machine
+        //machineController.resetMachine();
         machineController.startMachine();
+        //machineController.stopMachine();
+        //machineController.abortMachine();
+        //machineController.clearMachine();
 
         machineController.readStateCurrent();
         machineController.readProductsProcessed();
