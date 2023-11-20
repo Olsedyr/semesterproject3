3780eb01dcc380cc0debf1c415d31caf6fa5cc7a
emtk500 <nikolaj.martin@gmail.com>
2023-11-20 16:02:20 +0100

    Connected to database and setup users tabler

--- semesterproject3/backend/src/main/java/org/example/Semesterproject3Application.java
+++ semesterproject3/backend/src/main/java/org/example/Semesterproject3Application.java
@@ -1,7 +1,9 @@
 package org.example;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@PropertySource("classpath:application.properties")
 public class Semesterproject3Application {
 
 	public static void main(String[] args) {
