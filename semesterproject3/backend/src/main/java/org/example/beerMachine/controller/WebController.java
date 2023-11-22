4e212a8c6c6cba2255db3b711c6cbe44e6bc48ff
Olsedyr <56992952+Olsedyr@users.noreply.github.com>
2023-11-22 16:26:15 +0100

    .

---
+++ semesterproject3/backend/src/main/java/org/example/beerMachine/controller/WebController.java
@@ -0,0 +1,15 @@
+package org.example.beerMachine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String showLoginPage() {
+        return "GUI/LoginPage";
+    }
+
+
+}
