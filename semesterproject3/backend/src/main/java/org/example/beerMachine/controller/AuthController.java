42acf6fc991bca9ce23739d49dfa8f6a6e44de5b
emtk500 <nikolaj.martin@gmail.com>
2023-11-23 17:03:08 +0100

    nothing big

---
+++ semesterproject3/backend/src/main/java/org/example/beerMachine/controller/AuthController.java
@@ -0,0 +1,34 @@
+package org.example.beerMachine.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // You can generate a JWT token here and return it to the client
+        String jwtToken = "your_generated_jwt_token";
+
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwtToken));
+    }
+
+}
