74c6945706cbc1d77c7c0713b92bb11388b0a0b0
KHMaechel <kimmaechel@gmail.com>
2023-12-13 16:14:05 +0100

    Added recipe to CurrentBatchInfo

--- semesterproject3/frontend/src/components/CurrentBatchInfo.js
+++ semesterproject3/frontend/src/components/CurrentBatchInfo.js
@@ -22,6 +22,15 @@ const statusCodeTranslation = {
     19: 'Activating',
   };
 
+  const recipeTranslation = {
+    0: 'Pilsner',
+    1: 'Wheat',
+    2: 'IPA',
+    3: 'Stout',
+    4: 'Ale',
+    5: 'Alcohol Free',
+};
+
 const CurrentBatchInfo = () => {
     const [defectNodeValue, setDefectNodeValue] = useState(null);
     const [producedNodeValue, setProducedNodeValue] = useState(null);
@@ -30,7 +39,7 @@ const CurrentBatchInfo = () => {
     const [machineSpeedCurrentValue, setMachineSpeedCurrentValue] = useState(null);
     const [machineSpeedCurrentProductsPerMinute, setMachineSpeedCurrentProductsPerMinute] = useState(null);
     const [quantityCurrentValue, setQuantityCurrentValue] = useState(null);
-
+    const [recipeCurrentValue, setRecipeCurrentValue] = useState(null);
 
     useEffect(() => {
         const fetchData = async (url, setValue) => {
@@ -51,6 +60,7 @@ const CurrentBatchInfo = () => {
             { url: 'http://localhost:8080/opcua/machineSpeedCurrentSub', setValue: setMachineSpeedCurrentValue },
             { url: 'http://localhost:8080/opcua/machineSpeedCurrentProductsPerMinuteSub', setValue: setMachineSpeedCurrentProductsPerMinute },
             { url: 'http://localhost:8080/opcua/quantityCurrentSub', setValue: setQuantityCurrentValue },
+            { url: 'http://localhost:8080/opcua/recipeCurrentSub', setValue: setRecipeCurrentValue },
 
         ];
 
@@ -68,23 +78,31 @@ const CurrentBatchInfo = () => {
         if (stateCurrentValue !== null && statusCodeTranslation[stateCurrentValue] !== undefined) {
           return statusCodeTranslation[stateCurrentValue];
         }
-        return 'Loading...';
+        return 'null';
       };
+
+    const getRecipeDescription = () => {
+    if (recipeCurrentValue !== null && recipeTranslation[recipeCurrentValue] !== undefined) {
+        return recipeTranslation[recipeCurrentValue];
+    }
+    return 'null';
+    };
 
     return (
         <div className="info-box">
             <h2>Current batch</h2>
             <div className="box-content" id="currentBatchInfoBox">
 
-                <p><strong>Batch ID:</strong> {batchIdCurrentValue !== null ? batchIdCurrentValue : 'Loading...'}</p>
-                <p><strong>Batch Status:</strong> {stateCurrentValue}<strong>: </strong> {getStatusDescription()}</p>
+                <p><strong>Batch ID:</strong> {batchIdCurrentValue !== null ? batchIdCurrentValue : 'Null'}</p>
+                <p><strong>Batch Status (Value):</strong> {getStatusDescription()} ({stateCurrentValue})</p>
+                <p><strong>Recipe Name (ID):</strong> {getRecipeDescription()} ({recipeCurrentValue !== null ? recipeCurrentValue :  'Loading...'}) </p>
                 <p><strong>Total Processed Node Value:</strong> {producedNodeValue !== null ? producedNodeValue : 'Loading...'}</p>
                 <p><strong>Acceptable Node Value:</strong> {producedNodeValue - defectNodeValue !== null ? producedNodeValue - defectNodeValue : 'Loading...'}</p>
                 <p><strong>Defect Node Value:</strong> {defectNodeValue !== null ? defectNodeValue : 'Loading...'}</p>
                 <p><strong>Amounts To Produce:</strong> {quantityCurrentValue !== null ? quantityCurrentValue : 'Loading...'}</p>
                 <p><strong>Machine Speed Current Normalized Value (0-100):</strong> {machineSpeedCurrentValue !== null ? machineSpeedCurrentValue : 'Loading...'}</p>
                 <p><strong>Machine Speed Current (Products Per Minute):</strong> {machineSpeedCurrentProductsPerMinute !== null ? machineSpeedCurrentProductsPerMinute : 'Loading...'}</p>
-
+                
             </div>
         </div>
     );
